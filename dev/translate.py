7a0a197a5b8b4b2edeb0f9a3d95ba57bc0cc9bf8
patarapolw <patarapolw@gmail.com>
2018-11-21 15:20:00 +0700

    Add a lot of food

--- dev/translate.py
+++ dev/translate.py
@@ -4,31 +4,69 @@ from zhvocab.util import get_freq
 
 
 for row in '''
-流血
-肺核
-远视
-近视
-脑出血
-盲肠炎
-头晕
-发抖
-寒冷
-心脏衰竭
-累
-有食欲
-胃口好
-疲倦
-发炎
-食物中毒
-消化不良
-咳嗽
-泌尿道感染
-痔疮
+鱼鳔汤
+面条
+炸虾
+腊肠
+煎饺
+白菜豆腐汤
+枸杞炖鸡
+宫保鸡丁
+烧鸡
+烧卖
+面包
+甜点
+开胃菜
+米饭
+咖喱饭
+炒饭
+鸡蛋和培根
+火腿煎蛋
+粥
+海参汤
+清汤
+寿司
+三明治
+麻婆豆腐
+臭豆腐
+凉面
+熏鲑鱼
+清蒸鱼
+糖醋鱼
+蟹肉并
+北京烤鸭
+红烧暴雨
+泡菜
+炒青菜
+炒面
+佛跳墙
+鱼香肉丝
+披萨
+西红柿炒鸡蛋
+拉面
+牛排
+意大利面
+蔬菜沙拉
+寿喜烧
+小笼包
+香肠
+火锅
+麻辣烫
+烤乳猪
+鱼翅汤
+快餐
+海鲜
+素食
+热狗
+汉堡包
+早餐
+午餐
+晚餐
 '''.strip().split('\n'):
-    print(round(get_freq(row), 3))
+    # print(round(get_freq(row), 3))
 
-    # result = zh.Vocab.match(row.strip())
-    # if len(result) > 0:
-    #     print(result[0].pinyin)
-    # else:
-    #     print()
+    result = zh.Vocab.match(row.strip())
+    if len(result) > 0:
+        print(result[0].pinyin)
+    else:
+        print()
