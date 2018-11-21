32c14d389802447d5ade2d5f4e6174644050bb0d
patarapolw <patarapolw@gmail.com>
2018-11-21 09:43:41 +0700

    Update more diseases

--- dev/translate.py
+++ dev/translate.py
@@ -4,50 +4,31 @@ from zhvocab.util import get_freq
 
 
 for row in '''
-压力
-怀孕
-癌症
-传染
-感冒
-呕吐
-过敏
-糖尿病
-发烧
-头疼
-高血压
-流感
-肺炎
-失眠
-白血病
-性病
-哮喘
-腹泻
-天花
-痰
-肿胀
-打喷嚏
-痒痒
-便秘
-梅毒
-神经痛
-腹痛
-白内障
-低血压
-宿醉
-流鼻涕
-脓化
-足癣
-结膜炎
-荨麻疹
-小儿麻痹症
-消化性溃疡
-扁桃体炎
-龋齿
+流血
+肺核
+远视
+近视
+脑出血
+盲肠炎
+头晕
+发抖
+寒冷
+心脏衰竭
+累
+有食欲
+胃口好
+疲倦
+发炎
+食物中毒
+消化不良
+咳嗽
+泌尿道感染
+痔疮
 '''.strip().split('\n'):
-    # print(round(get_freq(row), 3))
+    print(round(get_freq(row), 3))
 
-    result = zh.Vocab.match(row.strip())
-    if len(result) > 0:
-        print(result[0].pinyin)
-    else:
-        print()
+    # result = zh.Vocab.match(row.strip())
+    # if len(result) > 0:
+    #     print(result[0].pinyin)
+    # else:
+    #     print()
